4a8122d7dbcc11b7d1808eafbacb936ea6a72efb
rajeshkrishnait <rajeshkrishnarajakumar@gmail.com>
2025-03-21 21:56:27 +0530

    add: second test added for returning same number if string has one number

--- calculator.test.ts
+++ calculator.test.ts
@@ -4,4 +4,7 @@ describe('test calcultor', ()=>{
     test('Empty string returns 0', ()=>{
         expect(add('')).toBe(0)
     })
+    test('single number should return the same number', ()=>{
+        expect(add('1')).toBe(1)
+    })
 })
