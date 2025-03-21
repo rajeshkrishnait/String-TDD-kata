ca6c490c2807abc79f40a21805446ec52ebec55f
rajeshkrishnait <rajeshkrishnarajakumar@gmail.com>
2025-03-21 22:00:35 +0530

    add: multi number testcase

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -3,6 +3,13 @@ export function add(numbers:string){
         return 0
     let delimeter = ','
     let numbersArray = numbers.split(delimeter).map(Number)
+    let sum = 0;
     if(numbersArray.length == 1)
         return numbersArray[0]
+    else{
+        numbersArray.forEach((number)=>{
+            sum+=number
+        })
+    }
+    return sum;
 }
--- calculator.test.ts
+++ calculator.test.ts
@@ -7,4 +7,7 @@ describe('test calcultor', ()=>{
     test('single number should return the same number', ()=>{
         expect(add('1')).toBe(1)
     })
+    test('two numbers should return the sum of the two numbers', ()=>{
+        expect(add('1, 4')).toBe(5)
+    })
 })
