5c048c480502f5989bd15646c203c367275cd44e
rajeshkrishnait <rajeshkrishnarajakumar@gmail.com>
2025-03-21 21:58:18 +0530

    add: second test passed

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,4 +1,8 @@
 export function add(numbers:string){
     if(!numbers)
         return 0
+    let delimeter = ','
+    let numbersArray = numbers.split(delimeter).map(Number)
+    if(numbersArray.length == 1)
+        return numbersArray[0]
 }
